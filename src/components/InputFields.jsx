e07a544389003c650826ab4a62aaba592db76e52
hpdrucker <hpdrucker@users.noreply.github.com>
2022-05-03 02:02:30 +0200

    fix grammar for inputfields

--- src/components/InputFields.jsx
+++ src/components/InputFields.jsx
@@ -10,27 +10,27 @@ const InputFields = () => {
         <div className='input-container'>
             <div className='inputs'>
             {speedInput}
-            <label className='labels' for='speed'>Max speed of cars in pixel per intervall           {/*  = {speed}    */}</label>
+            <label className='labels' for='speed'>Max speed of cars in pixel per interval</label>
             </div>
             <div className='inputs'>
             {lengthInput}
-            <label className='labels' for='length'>Road length in pixel   {/*  = {length}   */}</label>
+            <label className='labels' for='length'>Road length in pixel</label>
             </div>
             <div className='inputs'>
             {carsInput}
-            <label className='labels' for='cars'>Amount of cars on road       {/*  = {cars}     */}</label>
+            <label className='labels' for='cars'>Amount of cars on road</label>
             </div>
             <div className='inputs'>
             {retardedInput}
-            <label className='labels' for='retarded'>Chance of distracted drivers (1 = 100%){/*  = {retarded} */}</label>
+            <label className='labels' for='retarded'>Chance of distracted drivers (1 = 100%)</label>
             </div>
             <div className='inputs'>
                 {newCarChanceInput}
-                <label className='labels' htmlFor='retarded'>Chance of new Car (1 = 100%){/*  = {newCarChance} */}</label>
+                <label className='labels' htmlFor='newCarChance'>Chance of new Car (1 = 100%)</label>
             </div>
             <div className='inputs'>
             {intervalInput}
-            <label className='labels' for='interval'>Output intervall{/*  = {interval} */}</label>
+            <label className='labels' for='interval'>Output interval</label>
             </div>
         </div>
     )
